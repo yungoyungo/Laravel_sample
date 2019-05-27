f3b69f2c92ad9c8363524eb44f6614ad7ff426a2
yungo <29724679+yungoyungo@users.noreply.github.com>
2019-05-27 10:29:52 +0900

    use MAMP mysql

--- config/database.php
+++ config/database.php
@@ -49,6 +49,7 @@ return [
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
+            'unix_socket'   => '/Applications/MAMP/tmp/mysql/mysql.sock',
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
