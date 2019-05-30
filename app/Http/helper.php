5b5aae2a8a2ca7198d498e61c62a46a0237f1745
yungo <29724679+yungoyungo@users.noreply.github.com>
2019-05-30 10:30:37 +0900

    delete_formのヘルパー関数追加

---
+++ app/Http/helper.php
@@ -0,0 +1,10 @@
+<?php
+
+function delete_form($url, $label = '削除')
+{
+    $form = Form::open(['method' => 'DELETE', 'url' => $url, 'class' => 'd-inline']);
+    $form .= Form::submit($label, ['class' => 'btn btn-danger']);
+    $form .= Form::close();
+
+    return $form;
+}
