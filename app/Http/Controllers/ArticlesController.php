0f84a7b744f9e15a7abc3a6251a3b19431ba5f5b
yungo <29724679+yungoyungo@users.noreply.github.com>
2019-05-31 18:39:18 +0900

    fix controller

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Article;
+use App\Tag;
 use App\Http\Requests\ArticleRequest;
 use Carbon\Carbon;
 
@@ -36,7 +37,9 @@ class ArticlesController extends Controller
 
     public function create()
     {
-        return view('articles.create');
+        $tag_list = Tag::pluck('name', 'id');
+
+        return view('articles.create', compact('tag_list'));
     }
 
     // Illuminate\Http\Requestクラスのインスタンスを取得
@@ -61,22 +64,29 @@ class ArticlesController extends Controller
         //Article::create($request->validated());
 
         // ユーザーごとの記事として保存
-        Auth::user()->articles()->create($request->validated());
+        $article = Auth::user()->articles()->create($request->validated());
+        $article->tags()->attach($request->input('tags'));
 
         return redirect()->route('articles.index')->with('message', '記事を追加しました。');
     }
 
-    public function edit(Article $article) {
-        return view('articles.edit', compact('article'));
+    public function edit(Article $article)
+    {
+        $tag_list = Tag::pluck('name', 'id');
+
+        return view('articles.edit', compact('article', 'tag_list'));
     }
 
-    public function update(ArticleRequest $request, Article $article) {
+    public function update(Article $article, ArticleRequest $request)
+    {
         $article->update($request->validated());
+        $article->tags()->sync($request->input('tags'));
 
         return redirect()->route('articles.show', [$article->id])->with('message', '記事を修正しました。');
     }
 
-    public function destroy(Article $article) {
+    public function destroy(Article $article)
+    {
         $article->delete();
  
         return redirect()->route('articles.index')->with('message', '記事を削除しました。');
