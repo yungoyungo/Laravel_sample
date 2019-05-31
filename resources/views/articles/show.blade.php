2f5488fc97ec122145a7724167886477d5c9d0c3
yungo <29724679+yungoyungo@users.noreply.github.com>
2019-05-31 18:15:49 +0900

    修正

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -15,20 +15,18 @@
         <h5>Tags:</h5>
         <ul>
             @foreach($article->tags as $tag)
-                <li><{{ $tag->name }}/li>
+                <li>{{ $tag->name }}</li>
             @endforeach
         </ul>
     @endunless
  
     <br/>
  
-    <div>
-        @auth
-            <a href="{{ action('ArticlesController@edit', [$article->id]) }}" class="btn btn-primary">編集</a>
+    @auth
+        <a href="{{ action('ArticlesController@edit', [$article->id]) }}" class="btn btn-primary">編集</a>
 
-            {!! delete_form(['articles', $article->id]) !!}
-        @endauth
+        {!! delete_form(['articles', $article->id]) !!}
+    @endauth
 
-        <a href="{{ route('articles.index') }}" class="btn btn-secondary float-right">一覧へ戻る</a>
-    </div>
+    <a href="{{ route('articles.index') }}" class="btn btn-secondary float-right">一覧へ戻る</a>
 @endsection
